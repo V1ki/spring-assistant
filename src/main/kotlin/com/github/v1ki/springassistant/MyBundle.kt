5357f6a56cf03ef9ec6e1a14c9b8de8860333db5
GitHub Action <action@github.com>
2021-07-12 01:13:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/v1ki/springassistant/MyBundle.kt
+++ src/main/kotlin/com/github/v1ki/springassistant/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.v1ki.springassistant
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/v1ki/springassistant/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/v1ki/springassistant/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.v1ki.springassistant.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.v1ki.springassistant.MyBundle
 
 class MyApplicationService {
 
